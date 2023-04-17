31504a0b4d59044d42f1492f5c720d01cb7525ba
unknown <andhikawahyupratamasmart@gmail.com>
2023-04-17 13:56:29 +0700

    v 1.03

--- src/app.js
+++ src/app.js
@@ -4,20 +4,20 @@ const serverless=require("serverless-http");
 const router=express.Router();
 module.exports.handler=serverless(app);
 app.use("/.netlify/functions/app",router)
-app.use(express.static("src/dist"));
+app.use(express.static("dist"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "src/dist/index.html")
+    res.sendFile(__dirname + "index.html")
 })
 app.post("/", (req, res) => {
     res.redirect("/")
 })
 app.post("/short", (req, res) => {
-    res.sendFile(__dirname + "src/dist/short.html")
+    res.sendFile(__dirname + "/short.html")
 })
 
 app.post("/long", (req, res) => {
-    res.sendFile(__dirname + "src/dist/long.html")
+    res.sendFile(__dirname + "dist/long.html")
 })
 
 app.listen(3000, (req, res) => {
